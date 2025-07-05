a54d2888cad2b26e9991dc53ad65ce38e85876e2
sofia <sofymagalisalomon@gmail.com>
2025-07-05 03:56:31 +0000

    .

---
+++ ejercicio_01.py
@@ -0,0 +1,4 @@
+nombre="samuel"
+apellido="escobar"
+nombre_completo=f"{nombre}{apellido}"
+print(nombre_completo.title())
---
+++ ejercicio_02.py
@@ -0,0 +1,5 @@
+nombre="samuel"
+apellido="escobar"
+nombre_completo=f"{nombre} {apellido}"
+print(nombre_completo.upper())
+print(nombre_completo.lower())
