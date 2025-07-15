410a08ab0d8bdb878e55f2db19e1ce5ef0d8b698
sofia <sofymagalisalomon@gmail.com>
2025-07-15 16:44:04 +0000

    formateo de string

---
+++ u3_calculadora.py
@@ -0,0 +1,19 @@
+#enteros(int).
+x=int(input("¿cual es x?"))
+y=int(input("¿cual es y?"))
+z=x+y
+print(z)
+#float(valores decimales).
+a=float(input("¿cual es a?"))
+b=float(input("¿cual es b?"))
+print(a+b)
+#round(valores redondeados).
+#obtiene entradas.
+a=float(input("¿cual es a?"))
+b=float(input("¿cual es b?"))
+#redondea el numero.
+c=round(a+b)
+#resultado.Esto devuelve str por que esta envuelto en el formateo.
+print(f"{c:,}")
+
+#ahora con dividendo.
